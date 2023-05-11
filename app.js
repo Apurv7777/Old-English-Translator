026e8c2f0a1accb3af5757704e96b3cd97e1e7da
Apurv7777 <65327303+Apurv7777@users.noreply.github.com>
2023-05-11 13:12:29 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+var btnTranslate = document.querySelector("#btn-translate")
+
+var txtInput = document.querySelector("#txtInput")
+
+var output = document.querySelector(".output")
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getURL(text){
+    return serverUrl+"?"+"text="+text
+}
+
+function errorHandler(error){
+    console.log("Error :"+error)
+}
+
+function onClicked(){
+
+    var input = txtInput.value
+    fetch(getURL(input))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText;
+    })
+    .catch(errorHandler)
+    
+}
+
+btnTranslate.addEventListener("click",onClicked)
+
+
+
